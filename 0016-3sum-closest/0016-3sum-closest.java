0833153e56ad39abfb45e402e9e03d197b3b1a75
Mohd Riyaz <144448602+Riyaz082@users.noreply.github.com>
2026-08-18 20:21:42 +0530

    Time: 17 ms (78.06%), Space: 45.1 MB (95.25%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -8,10 +8,12 @@ class Solution {
             int right = n - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == target) return sum;
+                
                 if (Math.abs(sum - target) < Math.abs(closest - target)) {
                     closest = sum;
                 }
-                if(sum == target) return sum;
 
                 if(sum < target) left++;
                 else right--;
