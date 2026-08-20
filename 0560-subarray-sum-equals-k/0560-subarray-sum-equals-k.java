75a37f38bf17c80ce29fd243117490576648b3c1
Mohd Riyaz <144448602+Riyaz082@users.noreply.github.com>
2026-08-20 12:19:37 +0530

    Time: 26 ms (34.28%), Space: 48.5 MB (87.48%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, res = 0;
+        
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int see = sum - k;
+            int freq = map.getOrDefault(see, 0);
+            res += freq;
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
